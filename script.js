74ee4c8b3440cded94ebe832c17c101e32ac8da7
Ron Milman <milmanron@gmail.com>
2025-11-02 08:39:28 +0200

    refactor header and CSS, add footer and interactive improvements

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 const burger = document.querySelector(".burger");
-const menu = document.querySelector(".menu");
+const content = document.querySelector(".content");
+const header = document.querySelector("header");
 
 burger.addEventListener("click", () => {
   const isActive = burger.classList.contains("active");
   burger.classList.toggle("active");
-  menu.classList.toggle("open");
-  menu.classList.toggle("spacing-16");
   burger.setAttribute("aria-expanded", !isActive);
+
+  if (!header.classList.contains("expanded")) {
+    header.classList.add("expanded");
+    content.classList.remove("hidden");
+    setTimeout(() => {
+      content.classList.add("visible");
+    }, 150);
+  } else {
+    content.classList.remove("visible");
+    setTimeout(() => {
+      content.classList.add("hidden");
+      header.classList.remove("expanded");
+    }, 100);
+  }
 });
+
+document.addEventListener("click", (e) => {
+  const isActive = burger.classList.contains("active");
+
+  if (isActive &&  !burger.contains(e.target)) {
+    burger.classList.remove("active");
+    burger.setAttribute("aria-expanded", !isActive);
+    content.classList.remove("visible");
+    setTimeout(() => {
+      content.classList.add("hidden");
+      header.classList.remove("expanded");
+    }, 100);
+  }
+});
+
